8e6245616375d71fe33396a8f0a70cf419878f57
GYoung <ytok1108@aol.com>
2024-10-22 21:39:21 +0900

    client _ json___다시 정렬

--- 01_hyper_tutorial/a01_hello_hyper/crates/client_json/src/main.rs
+++ 01_hyper_tutorial/a01_hello_hyper/crates/client_json/src/main.rs
@@ -12,19 +12,6 @@ use tokiort::TokioIo;
 // A simple type alias so as to DRY.
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let url = "http://jsonplaceholder.typicode.com/users".parse().unwrap();
-    let users = fetch_json(url).await?;
-    // print users
-    println!("users: {:#?}", users);
-
-    // print the sum of ids
-    let sum = users.iter().fold(0, |acc, user| acc + user.id);
-    println!("sum of ids: {}", sum);
-    Ok(())
-}
-
 async fn fetch_json(url: hyper::Uri) -> Result<Vec<User>> {
     let host = url.host().expect("uri has no host");
     let port = url.port_u16().unwrap_or(80);
@@ -64,4 +51,17 @@ struct User {
     id: i32,
     #[allow(unused)]
     name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = "http://jsonplaceholder.typicode.com/users".parse().unwrap();
+    let users = fetch_json(url).await?;
+    // print users
+    println!("users: {:#?}", users);
+
+    // print the sum of ids
+    let sum = users.iter().fold(0, |acc, user| acc + user.id);
+    println!("sum of ids: {}", sum);
+    Ok(())
 }
