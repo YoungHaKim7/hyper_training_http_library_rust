524795936347bd98712b8dc7ae33e6298791d9c6
GYoung <ytok1108@aol.com>
2024-10-23 22:51:11 +0900

    tokio_test

--- 01_hyper_tutorial/README.md
+++ 01_hyper_tutorial/README.md
@@ -1,2 +1,4 @@
 # tokio-test
 - https://docs.rs/tokio/latest/tokio/index.html#unstable-features
+
+- https://docs.rs/tokio/latest/tokio/attr.test.html
---
+++ 01_hyper_tutorial/tokio_test/src/main.rs
@@ -0,0 +1,52 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("localhost:8080 🚀 🚀  127.0.0.1:8080");
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
+
+#[tokio::test]
+async fn my_test() {
+    assert!(true);
+}
+
+#[test]
+fn my_test_multi_thread() {
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(2)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            assert!(true);
+        })
+}
